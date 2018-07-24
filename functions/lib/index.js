4a1d0e914fd09cfed0ccca6998a984fc7f90596b
Shaun Hutch <shaunhutch@Shauns-MacBook-Pro.local>
2018-07-24 13:16:54 -0400

    new config

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,19 +1,19 @@
-const functions = require('firebase-functions');
-const { Nuxt } = require('nuxt');
-const express = require('express');
-const app = express();
-const config = {
+var functions = require('firebase-functions');
+var Nuxt = require('nuxt').Nuxt;
+var express = require('express');
+var app = express();
+var config = {
     dev: false,
     buildDir: 'nuxt',
     build: {
         publicPath: '/assets/'
     }
 };
-const nuxt = new Nuxt(config);
+var nuxt = new Nuxt(config);
 function handleRequest(req, res) {
     res.set('Cache-Control', 'public, max-age=150, s-maxage=150');
-    return new Promise((resolve, reject) => {
-        nuxt.render(req, res, promise => {
+    return new Promise(function (resolve, reject) {
+        nuxt.render(req, res, function (promise) {
             promise.then(resolve).catch(reject);
         });
     });
